0251c24648f0ce0bbb2d168ef676a4653bdc2cf1
abhishek12234 <abhishekindore786@gmail.com>
2024-12-16 03:12:15 +0530

    new commit

--- twSafelistGenerator/index.js
+++ twSafelistGenerator/index.js
@@ -1,17 +1,16 @@
-const fs = require('fs')
-const plugin = require('tailwindcss/plugin')
-const generator = require('./generator')
-const crypto = require('crypto')
+import fs from 'fs';
+import plugin from 'tailwindcss/plugin';
+import generator from './generator.js';
+import crypto from 'crypto';
 
-module.exports = plugin.withOptions(({ path = 'safelist.txt', patterns = [] }) => ({ theme }) => {
+export default plugin.withOptions(({ path = 'safelist.txt', patterns = [] }) => ({ theme }) => {
+    const safeList = generator(theme)(patterns).join('\n');
+    const currentSafeList = fs.readFileSync(path).toString();
 
-    const safeList = generator(theme)(patterns).join('\n')
-    const currentSafeList = fs.readFileSync(path).toString()
-
-    const hash = crypto.createHash('md5').update(JSON.stringify(safeList)).digest('hex')
-    const prevHash = crypto.createHash('md5').update(JSON.stringify(currentSafeList)).digest('hex')
+    const hash = crypto.createHash('md5').update(JSON.stringify(safeList)).digest('hex');
+    const prevHash = crypto.createHash('md5').update(JSON.stringify(currentSafeList)).digest('hex');
 
     if (hash !== prevHash) {
-        return fs.writeFileSync(path, safeList)
+        fs.writeFileSync(path, safeList);
     }
-})
+});
