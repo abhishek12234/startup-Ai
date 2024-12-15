a73afcec361724451e8c649e08574aafc11830f5
abhishek12234 <abhishekindore786@gmail.com>
2024-12-16 04:58:18 +0530

    hi
    hi

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import dynamicImport from 'vite-plugin-dynamic-import'
+import commonjs from "vite-plugin-commonjs"
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,7 +13,7 @@ export default defineConfig({
       ]
     }
   }),
-  dynamicImport()],
+  dynamicImport(),commonjs()],
   assetsInclude: ['**/*.md'],
 
   resolve: {
